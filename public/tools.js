f17be2276a41ba80cfd6c69044375aa54947347a
Chernuga <146464361+Chernuga@users.noreply.github.com>
2026-09-17 20:24:00 +0300

    Delete public directory

--- public/tools.js
+++
@@ -1,9 +0,0 @@
-// The order here is the order in the sidebar.
-window.TOOLS = [
-  { path: 'grades',      name: 'Grades',      icon: '📊' },
-  { path: 'assignments', name: 'Assignments', icon: '📝' },
-  { path: 'exams',       name: 'Exams',       icon: '📚' },
-  { path: 'absences',    name: 'Absences',    icon: '🏫' },
-  { path: 'lessons',     name: 'Lessons',     icon: '📖' },
-  { path: 'chat',        name: 'Chat',        icon: '💬' },
-];
